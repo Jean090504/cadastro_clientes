d1eae5c77db93b75ccb40dd315d508f7b7a79816
JEAN COSTA ALVES DA SILVA <25203640@CFP127ALUNO.LOCAL>
2025-11-24 08:05:42 -0300

    Iniciando Projeto

--- src/br/com/jean/cadastro/CadastroClientesApp.java
+++ src/br/com/jean/cadastro/CadastroClientesApp.java
@@ -1,4 +1,7 @@
 package br.com.jean.cadastro;
 
 public class CadastroClientesApp {
+    public static void main(String[] args) {
+        System.out.println("Cadastro de Clientes");
+    }
 }
--- src/br/com/jean/cadastro/model/Cliente.java
+++ src/br/com/jean/cadastro/model/Cliente.java
@@ -1,4 +1,16 @@
 package br.com.jean.cadastro.model;
 
+import java.time.LocalDate;
+
 public class Cliente {
+    private int id;
+    private String nome;
+    private String email;
+    private LocalDate dataNascimento;
+
+    public void exibirCliente() {
+
+    }
+
+
 }
